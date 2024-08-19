8eba556c547290da7d2e051892eb1a462254b270
Local_Sholum <localSholum@gmail.com>
2024-08-19 12:28:54 +0700

    update json & ts file

--- study_ts.ts
+++ study_ts.ts
@@ -4,5 +4,9 @@ console.log('hello world!!');
  * 2. inisialisasi projek dengan 'npm init'. command akan membuat file 'package.json' di direkori
  * 3. install TypeScript sebagai dependensi project 'npm install typescript --save-dev'
  * 4. inisiasi TypeScript Config', command akan menkofigurasi project ts & akan membuat file 'tsconfig.json'
- * 
+ * 5.1. kompilasi file .ts. dengan 'tsc nama-file.ts' hal ini akan membuat file yang sama dengan format 'js
+ * 5.2. kompilasi tanpa berganti format.
+ *      a. install paket 'ts-node' 'npm install -g ts-node'. install global btw
+ *      b. install paket 'ts-node' 'npm install ts-node --save-dev'. install tingkat project btw
+ *      c. jalankan dengan 'ts-node namaFile.ts'
  */
