540216912614387d79e49fa96ca9da5acb2b7c05
Local_Sholum <localSholum@gmail.com>
2024-08-19 12:17:34 +0700

    add json & ts file

---
+++ study_ts.js
@@ -0,0 +1,8 @@
+console.log('hello world!!');
+/**cara insall project dengan ts
+ * 1. buat folder & isinya
+ * 2. inisialisasi projek dengan 'npm init'. command akan membuat file 'package.json' di direkori
+ * 3. install TypeScript sebagai dependensi project 'npm install typescript --save-dev'
+ * 4. inisiasi TypeScript Config', command akan menkofigurasi project ts & akan membuat file 'tsconfig.json'
+ *
+ */ 
